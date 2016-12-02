abae2dc17fc20ded722e45e2e293e506dea0d1cb
eferrin <eferrin_93@hotmail.com>
2016-12-02 10:19:02 -0300

    Server Noticia: version1

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 
-const http = require('http');
-const port = 3000;
-var date = new Date();
+var http = require('http');
+var port = 3000;
+var fs = require('fs');
+var url = require("url");
 
+var server = http.createServer()
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-
-  console.log(date);
-  response.end(date.toString());
-}
-
-const server = http.createServer(requestHandler)
+server.on("request", function(req,res){
+  var urlData=url.parse(req.url);
+  var path = urlData.pathname;
+  if (path=="/"){
+    path="/index.html";
+  }
+  var filePath= "public"+path;
+  console.log(filePath);
+  fs.exists(filePath,function(exists){
+    if(exists){
+      fs.readFile("public"+path, function(err,data){
+        if (err){
+          res.writeHead(500);
+          res.end("Ha ocurrido algo malo")
+        }else{
+          res.end(data);
+        }
+      })
+    }else {
+      res.writeHead(404);
+      res.end("No existe")
+    }
+  });
+});
 
 server.listen(process.env.PORT || port, (err) => {
   if (err) {
---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+$(function(){
+  ManejadorArticulos.load();
+  ManejadorArticulos.render("articulos");
+});
